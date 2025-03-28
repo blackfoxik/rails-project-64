284cbe2bc71312e0e3651c89d9a99409ff5ff582
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2025-03-28 17:17:21 +0300

    tmp 32

--- Makefile
+++ Makefile
@@ -1,12 +1,5 @@
 render-build:
-  set -o errexit
-  bundle lock --add-platform x86_64-linux
-  bundle install
-  bundle exec rake assets:precompile
-  bundle exec rake assets:clean
-  ./node_modules/.bin/esbuild app/javascript/*.* --bundle --outdir=app/assets/builds
-  ./node_modules/.bin/tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css
-  bundle exec rake db:migrate
+   bin/render-build.sh
 
 render-start:
 	bundle exec puma -t 5:5 -p ${PORT} -e ${RACK_ENV}
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+# set -o errexit
 
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
+#!/usr/bin/env bash
+
+# exit on error
+
+set -o errexit
+bundle lock --add-platform x86_64-linux
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+./node_modules/.bin/esbuild app/javascript/*.* --bundle --outdir=app/assets/builds
+./node_modules/.bin/tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css
 bundle exec rake db:migrate
 bundle exec rake db:seed

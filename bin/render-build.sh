b185233ec8a3f679dcaa472011b5e51aed8b4387
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2025-03-28 17:43:14 +0300

    tmp 36

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -16,6 +16,7 @@ bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+yarn build:css
 #./node_modules/.bin/esbuild app/javascript/*.* --bundle --outdir=app/assets/builds
 #./node_modules/.bin/tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css
 bundle exec rake db:migrate

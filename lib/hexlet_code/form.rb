e03e3ebb4ba37cecdbec23e94e48df72a943c3b1
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2025-03-28 03:12:40 +0300

    tmp 16

--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -9,11 +9,11 @@ class Form < Element
     super(name: 'form', attributes:, content: [])
   end
 
-  def input(name, as: :input, *)
+  def input(name, as: :input, **kwargs)
     @element[:content] << Elements::Label.new(name)
     value = @entity.public_send(name)
     control_class = Elements.const_get(as.capitalize)
-    @element[:content] << control_class.new(name, value, *)
+    @element[:content] << control_class.new(name, value, **kwargs)
   end
 
   def submit(value = 'Save')
